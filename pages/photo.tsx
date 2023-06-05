d23e1631d8fdbb9b5335d098496bc0eebc10a426
tugusav <tugus.ibram@gmail.com>
2023-06-05 19:56:20 +0700

    removed submitPhoto function

--- pages/photo.tsx
+++ pages/photo.tsx
@@ -17,16 +17,16 @@ const WebcamPage: React.FC<WebcamPageProps> = () => {
     setCapturedImage(null);
   };
 
-  const submitPhoto = async () => {
-    try {
-      const response = await axios.post("your-api-endpoint", {
-        image: capturedImage,
-      });
-      console.log(response.data); // Do something with the API response
-    } catch (error) {
-      console.error(error);
-    }
-  };
+//   const submitPhoto = async () => {
+//     try {
+//       const response = await axios.post("your-api-endpoint", {
+//         image: capturedImage,
+//       });
+//       console.log(response.data); // Do something with the API response
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
 
   return (
     <div>
@@ -36,18 +36,18 @@ const WebcamPage: React.FC<WebcamPageProps> = () => {
             src={capturedImage}
             alt="Captured"
             className="rounded-xl"
-            width={1280}
-            height={720}
+            width={640}
+            height={640}
           />
           <div className="flex flex-row space-x-5 items-center justify-center">
           <button className="bg-white text-orange-500 hover:bg-gray-100 hover:text-orange-700  text-2xl font-bold py-10 px-40 rounded-3xl" onClick={retakePhoto}>Retake</button>
-          <button className="bg-orange-500 hover:bg-orange-700 text-white text-2xl font-bold py-10 px-40 rounded-3xl shadow-md" onClick={submitPhoto}>Submit</button>
+          <button className="bg-orange-500 hover:bg-orange-700 text-white text-2xl font-bold py-10 px-40 rounded-3xl shadow-md">Submit</button>
           </div>
         </div>
       ) : (
         // make webcam in the middle of screen
         <div className="flex flex-col space-y-5 items-center justify-center py-10">
-          <Webcam audio={false} mirrored={true} ref={webcamRef} width={1280} height={720} />
+          <Webcam audio={false} mirrored={true} ref={webcamRef} width={640} height={640} />
           <button
             className="bg-orange-500 hover:bg-orange-700 text-white text-2xl font-bold py-10 px-40 rounded-3xl"
             onClick={capturePhoto}
