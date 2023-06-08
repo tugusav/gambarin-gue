449a97fbe4471e31a6f37f8c23e94549c96a89ac
tugusav <tugus.ibram@gmail.com>
2023-06-08 23:40:44 +0700

    profile page fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
-function Header() {
+import React from "react";
+
+export default function Header() {
   return (
     <header className="flex p-5 justify-between items-center sticky top-0 z-50 bg-white shadow-md">
       {/* left */}
@@ -26,21 +28,24 @@ function Header() {
 
       {/* right */}
       <div className="flex text-xs md:text-base divide-x items-center text-gray-500">
-        <Link href="/my-images" className="px-2 font-bold text-right">
+        <Link href="/my-images" className="px-4 font-bold text-right">
           My Images
         </Link>
         <Link
           href="https://github.com/tugusav/gambarin-gue"
-          className="px-2 font-bold text-right"
+          className="px-4 font-bold text-right"
         >
-          Github Repo
+          Github
         </Link>
-          <Link href="/profile"
-           className="px-2 font-bold text-right"
-          >Profile</Link>
+        <Link href="/profile" className="px-4 font-bold">
+          <Image
+            src="https://cdn-icons-png.flaticon.com/128/1144/1144760.png"
+            alt="profile"
+            width={40}
+            height={40}
+          />
+        </Link>
       </div>
     </header>
   );
 }
-
-export default Header;
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -15,7 +15,7 @@ function Profile({}: any) {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        { user && <h1 className="p-4 text-3xl font-bold">Hello, {user.username} !</h1> }
+        { user && <h1 className="p-4 text-3xl font-bold">Hello, {user['username']} !</h1> }
         <Link href="/"
         className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded" onClick={() => Auth.signOut()}>Sign Out</Link>
     </div>
